ec75e6a11d892a1e33b6e8e1ca32e5b54ae8d0b1
Yusuf C <yusufcfly@gmail.com>
2022-06-16 21:05:44 +0200

    weather.js_02

--- weather.js
+++ weather.js
@@ -11,13 +11,43 @@ form.addEventListener("submit", (e) =>{
 });
 
 // function getWeatherDataFromApi(){}
-const getWeatherDataFromApi = () =>{
+const getWeatherDataFromApi = async() =>{
     // alert("http request gone");
     // input.value = "";
-    let apikey = DecryptStringAES(localStorage.getItem("apiKey"));
+    let tokenKey = DecryptStringAES(localStorage.getItem("apiKey"));
     // console.log(apikey);
     let inputVal = input.value;
-    let url = `https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
-    `;
+    let unitType = "metric";
+    let lang = "tr";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${tokenKey}&units=${unitType}&lang=${lang}`;
+
+    try {
+        // const response = await fetch.get(url).then(response => response.json());
+        //axios.get(url) == axios(url)
+        const response = await axios(url);
+        const { name, main, sys, weather } = response.data;
+        console.log(response.data);
+        let iconUrl = `http://openweathermap.org/img/wn/${weather[0].icon}@2x.png`;
+
+        const createdLi = document.createElement("li");
+        createdLi.classList.add("city");
+        const createdLiInnerHTML = 
+            `<h2 class="city-name" data-name="${name}, ${sys.country}">
+                <span>${name}</span>
+                <sup>${sys.country}</sup>
+            </h2>
+            <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
+            <figure>
+                <img class="city-icon" src="${iconUrl}">
+                <figcaption>${weather[0].description}</figcaption>
+            </figure>`;
+        createdLi.innerHTML = createdLiInnerHTML;
+        //append vs. prepend
+        list.append(createdLi);
+
+    } 
+    catch (error) {
+        
+    }
     form.reset();
 }
